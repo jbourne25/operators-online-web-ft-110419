ac7753118a401fb0207290107997371e4fa6dc26
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-06 03:10:50 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,8 @@
 def unsafe?(speed)
   if speed > 60 
-    returns true
+    returns "unsafe"
   elsif speed < 40
-    returns true
+    returns "unsafe"
   else 40 < speed > 60
     returns false
   end
